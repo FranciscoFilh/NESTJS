7e048aaff7c4ec7a4c6db9496a0ddbfd18986ec2
francisco oliveira <fs9457489@gmail.com>
2024-04-07 17:53:15 -0300

    NestJS + DTO + PostgreeSQL

--- second-project/src/User/user.service.ts
+++ second-project/src/User/user.service.ts
@@ -1,61 +1,93 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateUserDTO } from "./dto/create-user-dto";
-import { UpdateUserDTO } from "./dto/update-user-dto";
-import { UpdatePatchUserDTO } from "./dto/update-patch-user-dto";
+import { UpdateUserDTO } from "./dto/update-user-dto";  
+import { UpdatePatchUserDTO } from "./dto/update-patch-user-dto"; 
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {
+    constructor(private readonly prisma: PrismaService) {
 
-  }
+    }
 
-  async create({ name, email, cpf, password }: CreateUserDTO) {
-    return this.prisma.user_login.create({
-      data: {
-        name,
-        email,
-        cpf,
-        password
-      }
-    })
-  }
+    async create({name, email, cpf, password}: CreateUserDTO) {
+        return this.prisma.user_login.create({
+            data: {
+                name,
+                email,
+                cpf,
+                password,
+            }
+        })
+    }
 
-  async list() {
-    return this.prisma.user_login.findMany()
-  }
+    async list() {
+        return this.prisma.user_login.findMany()
+    }
 
-  async show(id: number) {
-    return this.prisma.user_login.findUnique({
-      where: {
-        id,
-      }
-    })
-  }
+    async show(id: number) {
+        return this.prisma.user_login.findUnique({
+            where: {
+                id,
+            }
+        })
+    }
 
-  async update(id: number, data: UpdateUserDTO) {
-    return this.prisma.user_login.update({
-      data,
-      where: {
-        id,
-      }
-    })
-  }
+    async update(id: number, data: UpdateUserDTO) {
+        if (!(await this.show(id))) {
+            throw new NotFoundException(`Usuário ${id} não encontrado!`)
+        }
+        return this.prisma.user_login.update({
+            data,
+            where: {
+                id,
+            }
+        })
+    }
 
-  async updatePartial(id: number, data: UpdatePatchUserDTO) {
-    return this.prisma.user_login.update({
-      data,
-      where: {
-        id
-      }
-    })
-  }
+    async updatePartial(id: number, {name, email, cpf, password}: UpdatePatchUserDTO) {
+        
+        const data: any = {}
 
-  async delete(id: number) {
-    return this.prisma.user_login.delete({
-      where: {
-        id,
-      }
-    })
-  }
+        if (!(await this.show(id))) {
+            throw new NotFoundException(`Usuário ${id} não encontrado!`)
+        }
+        
+        if (name) {
+            data.name = name
+        }
+
+        if (email) {
+            data.email = email
+        }
+
+        if (cpf) {
+            data.cpf = cpf
+        }
+
+        if (password) {
+            data.password = password
+        }
+
+
+        return this.prisma.user_login.update({
+            data,
+            where: {
+                id,
+            }
+        })
+    }
+
+    async delete(id: number) {
+        if (!(await this.show(id))) {
+            throw new NotFoundException(`Usuário ${id} não encontrado!`)
+        }
+        return this.prisma.user_login.delete({
+            where: {
+                id
+            }
+        }) 
+    }
+
+
 }
